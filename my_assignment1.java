1c9b22fc25a93e1eb2e78edd21bb9a88814ecd24
Abiola John Akinyele <105801057+AkinyeleAbiolaJohn@users.noreply.github.com>
2022-06-23 21:00:34 +0100

    Add files via upload

---
+++ my_assignment1.java
@@ -0,0 +1,13 @@
+public class my_assignment1 {
+    public static void main(String[] args){
+        for(int i = 0; i<=0; i++) {
+            for (int j = 0; j <= 7; j++) {
+                System.out.print("* ");
+            }
+            while (i <= 6) {
+                System.out.println("*");
+                i++;
+            }
+        }
+    }
+}
